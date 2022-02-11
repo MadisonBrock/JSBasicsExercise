219b0bfd17b5db92f9915f37d0f439aa54915aa0
Madison <madison.brock03@gmail.com>
2022-02-11 14:00:24 -0600

    Completed the exercise

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log(16);
+
+// Numbers, strings, booleans, null, undefined
+
+// Operating Symbols
+/*
++
+-
+*
+/
+**
+%
+*/
+
+console.log(5 % 2);//1
+console.log(5 % 4);//1
+
+console.log('I\'M MARY POPPINS, Y\'ALL!');// I'M MARY POPPINS, Y'ALL!
+
+console.log("hawk" + " " + "eye");//hawk eye
+
+console.log("madison brock".length);//13
+
+console.log("Avengers: Endgame" [16]);// e
